aebc2c59c6e56c24ccd0b1ae462a0ae6d903849e
wanghaoc <haoflynet@gmail.com>
2016-06-03 16:44:29 +0800

    添加web hook

--- admin/urls.py
+++ admin/urls.py
@@ -19,4 +19,5 @@ from django.contrib import admin
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^dashboard', include('dashboard.urls')),
+    url(r'^webhook', webhook.views.webhook),
 ]
---
+++ webhook/views.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+
+import system
+
+__author__ = 'haofly'
+__datetime__ = '2016-06-03 16:40:00'
+__description__ = 'github的webhook钩子'
+
+
+def webhook(request):
+    os.system('cd /var/www/admin')
+    os.system('git pull -f git@github.com:haoflynet/admin.git 2>&1')
