49e1b1f0e2c6db12c23066dd01ab7ce85eabf694
haoflynet <haoflynet@gmail.com>
2016-06-03 18:14:39 +0800

    设置web hook返回值

--- webhook/views.py
+++ webhook/views.py
@@ -1,7 +1,9 @@
 # -*- coding: utf-8 -*-
 
 import os
-from django.views.decorators.csrf import csrf_exempt                             
+from django.views.decorators.csrf import csrf_exempt
+from django.http import HttpResponse
+
 
 __author__ = 'haofly'
 __datetime__ = '2016-06-03 16:40:00'
@@ -12,3 +14,4 @@ __description__ = 'github的webhook钩子'
 def webhook(request):
     os.system('cd /var/www/admin')
     os.system('git pull -f git@github.com:haoflynet/admin.git 2>&1')
+    return HttpResponse('ok')
