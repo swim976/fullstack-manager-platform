1923c6ddb27be823d01e43c34a04a0299c965d83
wanghaoc <haoflynet@gmail.com>
2016-06-03 18:09:02 +0800

    web hook禁用csrf验证

--- webhook/views.py
+++ webhook/views.py
@@ -1,12 +1,14 @@
 # -*- coding: utf-8 -*-
 
 import os
+from django.views.decorators.csrf import csrf_exempt                             
 
 __author__ = 'haofly'
 __datetime__ = '2016-06-03 16:40:00'
 __description__ = 'github的webhook钩子'
 
 
+@csrf_exempt
 def webhook(request):
     os.system('cd /var/www/admin')
     os.system('git pull -f git@github.com:haoflynet/admin.git 2>&1')
