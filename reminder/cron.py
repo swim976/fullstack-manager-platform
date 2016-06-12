02a1e080b947424565908853a19eb110a429838b
haoflynet <haoflynet@gmail.com>
2016-06-13 07:53:44 +0800

    完成生日提醒功能

--- reminder/cron.py
+++ reminder/cron.py
@@ -2,7 +2,9 @@
 import datetime
 import re
 import requests
-#from . import service
+from people.models import People
+from django.db.models import Q
+from . import service
 __description__ = '各种定时任务'
 
 
@@ -22,45 +24,18 @@ def checkGithub():
 
 def checkBirth():
     '''查询明天生日的人，百度查询，今天是农历几月几日'''
-    url = 'https://www.baidu.com/s'
-    params = {
-        'ie': 'utf-8',
-        'f': '8',
-        'rsv_bp': '1',
-        'tn': 'baidu',
-        'wd': '今天是农历几月几日',
-        'oq': '今天是农历几月几日',
-        'rsv_pq': 'bf14469c00210a2d',
-        'rsv_t': 'dd6cJS7L8heOZxh2qjKYMqdjYTBoWZq+cM9MUxWIN2Hu3ewz9C8v3fYL2JI',
-        'rqlang': 'cn',
-        'rsv_enter': '1',
-        'rsv_sug3': '1',
-        'rsv_sug1': '1',
-        'rsv_sug7': '100',
-        'bs': '今天是农历几月几日'
-    }
-    headers = {
-        'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.84 Safari/537.36'
-    }
-    r = requests.get(url, params=params, headers=headers)
-    match = re.search('<div class="op-calendar-title c-gray">万年历</div>([\s\S]*?)<span>([\s\S]*?)<span>(?P<date>[\s\S]*?)</span>', r.text)
-    date = match.group('date') \
-                .replace(' ', '').replace('\n', '').split('农历')[1]
-    month, day = date.split('月')
-    months = [
-            '农历月', '一', '二', '三', '四', '五', '六',
-            '七', '八', '九', '十', '十一', '十二']
-    days = [
-            '农历天', '初一', '初二', '初三', '初四', '初五', '初六', '初七',
-            '初八', '初九', '初十', '十一', '十二', '十三', '十四', '十五', '十六',
-            '十七', '十八', '十九', '二十', '廿一', '廿二', '廿三', '廿四', '廿五',
-            '廿六', '廿七', '廿八', '廿九', '三十']
-    month = months.index(month)
-    day = days.index(day)
-    print(month, day)
+    old_month, old_day = service.getLunar()
+    today = datetime.date.today()
+    new_month, new_day = today.month, today.day
 
-
-
-
-if __name__ == '__main__':
-    checkBirth()
+    # 提示最近七天的生日
+    peoples = []
+    for _i in range(7):
+        peoples.append(People.objects.filter(
+            (Q(birth_lunar__month=old_month) & Q(birth_lunar__day=old_day)) |
+            (Q(birth_new__month=new_month) & Q(birth_new__day=new_day))
+        ))
+        old_month, old_day = service.getNextDate(old_month, old_day, 1)
+        new_month, new_day = service.getNextDate(new_month, new_day, 1)
+    email = service.generateBirthEmail(peoples)
+    service.sendEmail(email, '定时任务-生日提醒')
