f5ed6b2ec8c331c841eba167a60dd04ed4ba963a
haoflynet <haoflynet@gmail.com>
2016-06-14 22:41:51 +0800

    添加认证app

---
+++ oauth/urls.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+from django.conf.urls import url
+from . import views
+
+urlpatterns = [
+    url(r'^login$', views.login),      # 获取token
+    url(r'^get_token_info$', views.getTokenInfo),   # 获取信息查询接口
+    url(r'^revokeoauth$', views.revokeOauth),       # 授权回收接口
+]
