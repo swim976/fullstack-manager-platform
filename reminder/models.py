cfee06d47377bcdeef034e22d5f0ff8406c22c21
wanghaoc <haoflynet@gmail.com>
2016-07-08 23:17:32 +0800

    add  proxy str function

--- reminder/models.py
+++ reminder/models.py
@@ -12,3 +12,6 @@ class Proxy(models.Model):
     action = models.CharField('get/post支持', max_length=20)
     address = models.CharField('位置', max_length=50)
     created_at = models.DateTimeField('添加时间', auto_now_add=True)
+
+    def __str__(self):
+        return self.ip
