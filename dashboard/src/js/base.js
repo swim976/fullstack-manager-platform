cb2d4b5cfe74e542d411195565cadbfa4403134b
haoflynet <haoflynet@gmail.com>
2016-08-20 21:48:01 +0800

    update all static file

---
+++ dashboard/src/js/base.js
@@ -0,0 +1,7 @@
+// 每个页面都必须包含的
+
+var $ = require('gentelella/vendors/jquery/dist/jquery.min.js');
+global.jQuery = $;
+global.$ = $;
+require('gentelella/vendors/bootstrap/dist/js/bootstrap.min.js');
+require('gentelella/build/js/custom.min.js');
