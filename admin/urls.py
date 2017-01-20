818cba80192b15af165c33df87e496fa74d3faf3
wanghaoc <wanghaoc@zbj.com>
2017-01-20 17:43:42 +0800

    update README

--- admin/urls.py
+++ admin/urls.py
@@ -1,19 +1,3 @@
-from webhook import views as webhook_view
-"""admin URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/1.9/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  url(r'^$', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  url(r'^$', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.conf.urls import url, include
-    2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
-"""
 from django.conf.urls import include, url
 from django.contrib import admin
 
@@ -21,6 +5,5 @@ urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^dashboard/', include('dashboard.urls', namespace='dashboard')),
     url(r'^people/', include('people.urls', namespace='people')),
-    url(r'^webhook', webhook_view.webhook),
     url(r'^oauth/', include('oauth.urls', namespace='oauth')),
 ]
