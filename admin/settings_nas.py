56b3cf06d636cd56282b55c5d4a85e615f796606
haoflynet <haoflynet@gmail.com>
2017-05-02 18:47:17 +0800

    update was settings

--- admin/settings_nas.py
+++ admin/settings_nas.py
@@ -126,4 +126,14 @@ CRONJOBS = [
     ('0 5 * * *', 'cron.backup.database.Database'),
 ]
 
+# database备份目录
 BACKUP_DIR = '/data/backup/'
+
+# Sockes5代理
+SOCKS5_PROXY = '192.168.0.8:1086'
+
+# SPLASH服务
+SPLASH_HAR_URL = 'http://127.0.0.1:8050/render.har?'
+SPLASH_HTML_URL = 'http://127.0.0.1:8050/render.html?'
+SPLASH_HTTPS_HAR_URL = 'http://127.0.0.1:8051/render.har?'
+SPLASH_HTTPS_HTML_URL = 'http://127.0.0.1:8051/render.html？'
