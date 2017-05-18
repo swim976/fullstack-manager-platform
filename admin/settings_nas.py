c8772e806ce40a16e64188ad0a36c2fc3cbba006
haoflynet <haoflynet@gmail.com>
2017-05-18 09:55:04 +0800

    add settings of gas

--- admin/settings_nas.py
+++ admin/settings_nas.py
@@ -124,6 +124,7 @@ CRONJOBS = [
     ('30 11 * * *', 'cron.reminder.birthday.Birth'),
     ('0 1 * * *', 'cron.sign.smzdm.Smzdm'),
     ('0 5 * * *', 'cron.backup.database.Database'),
+    ('0 10 * * *', 'cron.crawler.fcw7.Fcw7'),
 ]
 
 # database备份目录
@@ -133,7 +134,7 @@ BACKUP_DIR = '/data/backup/'
 SOCKS5_PROXY = '192.168.0.8:1086'
 
 # SPLASH服务
-SPLASH_HAR_URL = 'http://127.0.0.1:8050/render.har?'
-SPLASH_HTML_URL = 'http://127.0.0.1:8050/render.html?'
-SPLASH_HTTPS_HAR_URL = 'http://127.0.0.1:8051/render.har?'
-SPLASH_HTTPS_HTML_URL = 'http://127.0.0.1:8051/render.html？'
+SPLASH_HAR_URL = 'http://192.168.0.9:8050/render.har?'
+SPLASH_HTML_URL = 'http://192.168.0.9:8050/render.html?'
+SPLASH_HTTPS_HAR_URL = 'http://192.168.0.9:8051/render.har?'
+SPLASH_HTTPS_HTML_URL = 'http://192.168.0.9:8051/render.html？'
