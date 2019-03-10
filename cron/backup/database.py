70ca22bd6e38089679f0deb75284a912fce74a70
haoflynet <haoflynet@gmail.com>
2019-03-10 20:51:05 +0800

    add ali db backup

--- cron/backup/database.py
+++ cron/backup/database.py
@@ -12,12 +12,25 @@ class Database(Cron):
 
     def __init__(self):
         super().__init__()
-        command = 'mysqldump -u{user} -p{password} --databases -h{host} {database} | gzip > {target}'.format(
+
+        # 备份nas数据库
+        command = 'mysqldump -u{user} -p{password} --databases -h{host} {backupdb} | gzip > {target}'.format(
             user=settings.DATABASES['default']['USER'],
             password=settings.DATABASES['default']['PASSWORD'],
             host=settings.DATABASES['default']['HOST'],
-            database=settings.DATABASES['default']['NAME'],
-            target=settings.BACKUP_DIR + 'mysql/' + str(datetime.date.today()) + '.sql.gz'
+            backupdb=settings.BACKUP_BACKUP_DB_NAS,
+            target=settings.BACKUP_DIR + 'mysql/nas' + str(datetime.date.today()) + '.sql.gz'
+        )
+        re = subprocess.check_output(command, shell=True)
+        print(re)
+
+        # 备份ali数据库
+        command = 'mysqldump -u{user} -p{password} --databases -h{host} {backupdb} | gzip > {target}'.format(
+            user=settings.DATABASES['ali']['ALI_USER'],
+            password=settings.DATABASES['ali']['PASSWORD'],
+            host=settings.DATABASES['ali']['HOST'],
+            backupdb=settings.BACKUP_DB_ALI,
+            target=settings.BACKUP_DIR + 'mysql/ali' + str(datetime.date.today()) + '.sql.gz'
         )
         re = subprocess.check_output(command, shell=True)
         print(re)
